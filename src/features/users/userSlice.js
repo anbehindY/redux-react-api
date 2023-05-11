8136af1fa4f494281c86a61e480610848d3aec4d
anbehindY <yanminthwin@gmail.com>
2023-05-11 17:46:39 +0630

    Create async thunk in userSlice

--- src/features/users/userSlice.js
+++ src/features/users/userSlice.js
@@ -1,5 +1,19 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
+const GET_URL = 'https://randomuser.me/api/?results=5';
+const fetchUsers = createAsyncThunk (
+    'users/fetchUsers',
+    async () => {
+        try {
+            const response = await axios.get(GET_URL);
+            return [...response.results];
+        } catch (error) {
+            return error.message;
+        }
+    }
+)
 const initialState = {
     users: [],
     isLoading: false,
